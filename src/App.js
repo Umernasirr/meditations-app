34c358563744e37c57c5e6e243f745de972d6eb2
Talal Abbas <talalabbas84@gmail.com>
2021-07-25 20:38:33 +0500

    Routing completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import Login from "./containers/Login";
 import { Flex } from "@chakra-ui/layout";
 
 function App() {
   return (
-    <Flex h="100vh" w="100vw">
-      <Login />
+    <Router>
+        <Flex h="100vh" w="100vw">
+        <Switch>
+          <Route exact path ='/login' component={Login} />
+        </Switch>
     </Flex>
+      </Router>
   );
 }
 
