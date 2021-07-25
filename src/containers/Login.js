ac9599feb5460d0aa597989468852009cb0d4776
Umer Nasir <umer.nasir@systemsltd.com>
2021-07-25 20:19:27 +0500

    Login PagE

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,117 @@
-import { Button } from "@chakra-ui/button";
-import { Flex } from "@chakra-ui/layout";
-import React from "react";
+import React, { useState } from "react";
 
+import {
+  Flex,
+  Text,
+  Box,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  InputLeftElement,
+} from "@chakra-ui/react";
+
+import {
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+  AiOutlineKey,
+  AiOutlineMail,
+} from "react-icons/all";
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleForgetPassword = () => {};
+
+  const handleCreateAccount = () => {};
+
+  const handleSubmit = () => {
+    console.log("Clicked");
+  };
+
   return (
-    <Flex h="full" w="full" bg="gray.200" align="center" justify="center">
-      <Button bg="brand.900" color="white">
-        CLick Me
-      </Button>
+    <Flex h="full" w="full" bg="gray.900" align="center" justify="center">
+      <Flex
+        w="30%"
+        h="50%"
+        bg="white"
+        align="center"
+        justify="center"
+        borderRadius={16}
+        direction="column"
+      >
+        <Text fontSize="2xl" fontWeight="bold">
+          Login
+        </Text>
+        <Box my={2} />
+        <InputGroup w="80%">
+          <InputLeftElement px={2}>
+            <AiOutlineMail onClick={() => setShowPassword(!showPassword)} />
+          </InputLeftElement>
+
+          <Input borderColor="gray.300" placeholder="Enter your Email" />
+        </InputGroup>
+
+        <Flex w="80%" align="left">
+          <Text color="red.700">{emailError}</Text>
+        </Flex>
+
+        <Box my={2} />
+        <InputGroup w="80%">
+          <InputLeftElement px={2}>
+            <AiOutlineKey onClick={() => setShowPassword(!showPassword)} />
+          </InputLeftElement>
+
+          <Input
+            type={showPassword ? "text" : "password"}
+            borderColor="gray.300"
+            placeholder="Enter your Password"
+          />
+
+          <InputRightElement px={2}>
+            {showPassword ? (
+              <AiOutlineEyeInvisible
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            ) : (
+              <AiOutlineEye onClick={() => setShowPassword(!showPassword)} />
+            )}
+          </InputRightElement>
+        </InputGroup>
+
+        <Flex w="80%" align="left">
+          <Text color="red.700">{passwordError}</Text>
+        </Flex>
+
+        <Box my={3} />
+
+        <Button width="120px" colorScheme="facebook" onClick={handleSubmit}>
+          Login
+        </Button>
+
+        <Box my={3} />
+
+        <Flex w="full" justify="space-around">
+          <Button
+            variant="link"
+            _focus={{ outline: "none" }}
+            color="facebook.300"
+            onClick={handleForgetPassword}
+          >
+            Forget Password
+          </Button>
+
+          <Button
+            variant="link"
+            _focus={{ outline: "none" }}
+            color="facebook.300"
+            onClick={handleCreateAccount}
+          >
+            Create an Account
+          </Button>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
